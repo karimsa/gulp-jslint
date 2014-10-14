fc76f159ed104359942698a889658e4926aee10c
Karim Alibhai <k4rim.sa@gmail.com>
2014-10-14 19:56:28 -0400

    fix broken tests

--- test/test-gulp.jslint.js
+++ test/test-gulp.jslint.js
@@ -72,10 +72,12 @@
         };
 
     lint('with good code', 'test-good.js');
-    lint('with good code (with shebang)', 'test-shebang.js');
     lint('with bad code', 'test-nomen.js').fail();
     lint('with directives', 'test-nomen.js', {
         nomen: true
+    });
+    lint('with good code (with shebang)', 'test-shebang.js', {
+        node: true
     });
 
     test('stream support', function (t) {

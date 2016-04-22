3b055852bc8f2e8a62f4c8604a7168d0caa4ed13
Karim Alibhai <k4rim.sa@gmail.com>
2016-04-21 21:27:20 -0400

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,54 +5,36 @@
  * Copyright (C) 2014 KarimSa Networks.
  **/
 
-(function () {
-    "use strict";
+'use strict';
 
-    var gulp = require('gulp'),
-        jslint = require('./gulp.jslint.js');
+var gulp = require('gulp'),
+    jslint = require('./gulp-jslint.js');
 
-    // lint all code
-    gulp.task('default', function () {
-        return gulp.src(['gulpfile.js', 'gulp.jslint.js'])
+// lint all code
+gulp.task('default', function () {
+    return gulp.src(['gulpfile.js', 'gulp-jslint.js'])
 
-            // pass your directives
-            // as an object
-            .pipe(jslint({
-                // these directives can
-                // be found in the official
-                // JSLint documentation.
-                node: true,
-                nomen: true,
+        // pass your directives
+        // as an object
+        .pipe(jslint({
+            // these directives can
+            // be found in the official
+            // JSLint documentation.
+            node: true,
+            nomen: true,
 
-                // you can also set global
-                // declarations for all source
-                // files like so:
-                global: [],
-                predef: [],
+            // you can also set global
+            // declarations for all source
+            // files like so:
+            global: [],
+            predef: []
 
-                // both ways will achieve the
-                // same result; predef will be
-                // given priority because it is
-                // promoted by JSLint
+            // both ways will achieve the
+            // same result; predef will be
+            // given priority because it is
+            // promoted by JSLint
+        }))
 
-                // pass in your prefered
-                // reporter like so:
-                reporter: 'default',
-
-                // specify whether or not
-                // to show 'PASS' messages
-                // for built-in reporter
-                errorsOnly: false
-            }))
-
-            // error handling:
-            // to handle on error, simply
-            // bind yourself to the error event
-            // of the stream, and use the only
-            // argument as the error object
-            // (error instanceof Error)
-            .on('error', function (error) {
-                console.error(String(error));
-            });
-    });
-}());
+        // pass in your prefered reporter like so:
+        .pipe(jslint.reporter('default', true));
+});
